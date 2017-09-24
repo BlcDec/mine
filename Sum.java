343a61936d14c686a81684bb2c097288310cabe1
DaLianMiao <zxc8750618>
2017-09-24 17:22:47 +0800

    java first time easy experiment

---
+++ Sum.java
@@ -0,0 +1,13 @@
+package SmallKit;
+
+public class Sum {
+	/*public static void main(String[] args) 
+	{
+		int ans=0;
+		for(int i=1;i<=6;i++)
+		{
+			ans+=i*11;
+		}
+		System.out.println(ans);
+	}*/
+}
